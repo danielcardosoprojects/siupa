95d587f3dacec7d6877980e96a47045d70df1e9b
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-08-31 13:20:17 -0300

    tt

---
+++ siiupa/router.php
@@ -0,0 +1,12 @@
+<?php
+// siiupa/router.php
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if (php_sapi_name() === 'cli-server') {
+    $file = __DIR__ . $path;
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/index.php';

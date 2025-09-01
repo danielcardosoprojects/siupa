3ee434df3f51f0109cc624a946c9cbebe5c08c0b
danielcardosoprojects <121764444+danielcardosoprojects@users.noreply.github.com>
2025-09-01 15:05:10 -0300

    Update index.php

--- siiupa/index.php
+++ siiupa/index.php
@@ -1,16 +1,16 @@
 ﻿<?php
 session_start();
 
-//header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 ini_set('display_errors', 0);       // Oculta erros na página
 ini_set('log_errors', 1);           // Ativa o registro de erros em log
 ini_set('error_log', '/path/to/your/php-error.log'); // Especifique o caminho do arquivo de log
 error_reporting(E_ALL);             // Gera todos os tipos de erros
 //header('Content-Type: text/html; charset=utf-8');
 include("bd/conectabd.php");
-//echo ($_SERVER['DOCUMENT_ROOT'] . '/conexao/verifica_login.php');
+echo ($_SERVER['DOCUMENT_ROOT'] . '/conexao/verifica_login.php');
 
-//include($_SERVER['DOCUMENT_ROOT'] . '/conexao/verifica_login.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/conexao/verifica_login.php');
 
 ?>
 
